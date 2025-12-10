a4939bec439a6f4884caa6e0c41da19aea1375e0
Jatin Dayama <jatindayama123@gmail.com>
2025-12-10 16:14:59 +0530

    rect area peri

---
+++ geometry.py
@@ -0,0 +1,6 @@
+def rect_area(len, br):
+    print("rect area : ", len*br)
+
+def rect_peri(len, br):
+    print("rect peri : ", 2*(len+br))
+
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import arithmetic
-print("Hello world !")
+from geometry import rect_peri
+
+print("Hello world!")
+
+length = 10
+breadth = 5
+
+print(rect_peri(length, breadth))
